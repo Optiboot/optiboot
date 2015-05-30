bfb3d5fc33a495e33a6cc591e2c4b7820aeb8710
WestfW <westfw@gmail.com>
2015-05-29 22:26:45 -0700

    Apparently working release.sh, .json.TEMPLATE and boards.txt !

--- optiboot/release.sh
+++ optiboot/release.sh
@@ -13,26 +13,31 @@ rm -Rf /tmp/optiboot-release
 
 #
 # Create the 3rd-party hardware extension directory structure
-mkdir /tmp/optiboot-release
-mkdir /tmp/optiboot-release/Optiboot
-mkdir /tmp/optiboot-release/Optiboot/avr
-mkdir /tmp/optiboot-release/Optiboot/avr/bootloaders
-mkdir /tmp/optiboot-release/Optiboot/avr/bootloaders/optiboot
-cp ../../boards-1.6.txt /tmp/optiboot-release/Optiboot/avr/boards.txt
+# Note that the structure under "packages" (handed by boards manager)
+# is different than the structure under "sketchbook/hardware" would be.
+
+mkdir -p /tmp/optiboot-release/Optiboot/bootloaders/optiboot
+TOP=/tmp/optiboot-release/Optiboot/
+cp ../../boards-1.6.txt $TOP/boards.txt
+
 #
 # Create platform.tx, because it contains the "group" name for the boards menu
-echo name=\[Optiboot $1\] > /tmp/optiboot-release/Optiboot/avr/platform.txt
-echo version=$1 >> /tmp/optiboot-release/Optiboot/avr/platform.txt
+echo name=Optiboot $1 > $TOP/platform.txt
+echo version=$1 >> $TOP/platform.txt
 
 #
 # Create a README file.
-echo This is an Optiboot version $1 \"Binary\" Release. > /tmp/optiboot-release/Optiboot/README.TXT
-echo >> /tmp/optiboot-release/Optiboot/README.TXT
-echo For Source code see http://github.com/Optiboot/optiboot >> /tmp/optiboot-release/Optiboot/README.TXT
+echo This is an Optiboot version $1 \"Binary\" Release. > $TOP/README.TXT
+echo >> $TOP/README.TXT
+echo For Source code see http://github.com/Optiboot/optiboot>> $TOP/README.TXT
 
 #
 # Copy over our "binaries."
-cp *.hex /tmp/optiboot-release/Optiboot/avr/bootloaders/optiboot
+cp *.hex $TOP/bootloaders/optiboot
+
+#
+# Copy examples ?  (examples for a boards.txt don't seem possible?)
+
 
 # files we'd specifical exclude, if we weren't doing only .hex files.
 #rm /tmp/optiboot-release/Optiboot/avr/bootloaders/optiboot/*.lst
@@ -43,5 +48,11 @@ cp *.hex /tmp/optiboot-release/Optiboot/avr/bootloaders/optiboot
 # zip everything up.
 pushd /tmp/optiboot-release
 zip -r Optiboot.zip Optiboot
+HASH=`openssl dgst -sha256 Optiboot.zip | sed -e 's/.* //'`
+SIZE=`stat -f %z Optiboot.zip`
+echo size = $SIZE
+echo hash = $HASH
 popd
+echo sed -e "s/%HASH%/$HASH/" -e "s/%VERSION%/$1/" -e "s/%SIZE%/$SIZE/" ../../package_optiboot_optiboot-additional_index.json.TEMPLATE
+sed -e "s/#.*//" -e "s/%HASH%/$HASH/" -e "s/%VERSION%/$1/" -e "s/%SIZE%/$SIZE/" ../../package_optiboot_optiboot-additional_index.json.TEMPLATE > /tmp/optiboot-release/package_optiboot_optiboot-additional_index.json
 
